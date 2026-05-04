3684de997b3646363986d7247f03c5bea2e20b40
Vidii06 <144632811+Vidii06@users.noreply.github.com>
2026-05-05 00:55:30 +0100

    mais coisas

--- Assets/Scripts/CollectButton.cs
+++ Assets/Scripts/CollectButton.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class CollectButton : MonoBehaviour
 {
+    public GameObject ButtonUI;
+    [SerializeField] private float collectEnableDelay = 0.1f;
+
+    private bool canCollect;
+
+    private void Start()
+    {
+        Invoke(nameof(EnableCollect), collectEnableDelay);
+    }
+
+    private void EnableCollect()
+    {
+        canCollect = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!canCollect)
+            return;
+
         if (other.CompareTag("Player"))
         {
             Debug.Log("BOTÃO COLETADO!");
@@ -11,6 +29,8 @@ public class CollectButton : MonoBehaviour
             UIButtonsManager.Instance.AddButton();
 
             Destroy(gameObject);
+
+            ButtonUI.GetComponent<Animator>().Play("OneButton");
         }
     }
 }
