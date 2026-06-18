b0d951dc596c1023964c7634e2887ceb56d8d29c
liaakk <117382298+liaakk@users.noreply.github.com>
2026-06-18 04:27:49 +0100

    logica notebook

--- Assets/Scripts/TabsManager.cs
+++ Assets/Scripts/TabsManager.cs
@@ -1,29 +1,65 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotebookTabs : MonoBehaviour
 {
+    [Header("Pages")]
     public GameObject objectsPage;
     public GameObject charactersPage;
     public GameObject cutscenesPage;
 
+    [Header("Buttons")]
+    public Image objectsButton;
+    public Image charactersButton;
+    public Image cutscenesButton;
+
+    [Header("Colors")]
+    public Color normalColor = Color.white;
+    public Color selectedColor = Color.gray;
+
+    void OnEnable()
+    {
+        ShowObjects(); // sempre abre na página objects
+    }
+
     public void ShowObjects()
     {
-        objectsPage.SetActive(true);
-        charactersPage.SetActive(false);
-        cutscenesPage.SetActive(false);
+
+{
+    Debug.Log("CLICK OBJECTS");
+    SetPages(true, false, false);
+    SetButtons(objectsButton);
+}
+        SetPages(true, false, false);
+        SetButtons(objectsButton);
     }
 
     public void ShowCharacters()
     {
-        objectsPage.SetActive(false);
-        charactersPage.SetActive(true);
-        cutscenesPage.SetActive(false);
+         Debug.Log("CLICK CHARACTERS FUNCIONA");
+        SetPages(false, true, false);
+        SetButtons(charactersButton);
     }
 
     public void ShowCutscenes()
     {
-        objectsPage.SetActive(false);
-        charactersPage.SetActive(false);
-        cutscenesPage.SetActive(true);
+        SetPages(false, false, true);
+        SetButtons(cutscenesButton);
+    }
+
+    void SetPages(bool obj, bool cha, bool cut)
+    {
+        objectsPage.SetActive(obj);
+        charactersPage.SetActive(cha);
+        cutscenesPage.SetActive(cut);
+    }
+
+    void SetButtons(Image activeButton)
+    {
+        objectsButton.color = normalColor;
+        charactersButton.color = normalColor;
+        cutscenesButton.color = normalColor;
+
+        activeButton.color = selectedColor;
     }
 }
