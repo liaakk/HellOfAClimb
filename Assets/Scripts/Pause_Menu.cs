0bd62f70e380f81fc2656f84ee4df199d8e751fc
liaakk <117382298+liaakk@users.noreply.github.com>
2026-06-18 02:04:00 +0100

    posicionamento e sombras lindas

--- Assets/Scripts/Pause_Menu.cs
+++ Assets/Scripts/Pause_Menu.cs
@@ -1,15 +1,31 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Pause_Menu : MonoBehaviour
 {
     public GameObject pausePanel;
+    private bool isPaused = false;
 
     void Update()
     {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        if (Keyboard.current.escapeKey.wasPressedThisFrame && !isPaused)
         {
-            pausePanel.SetActive(true);
+            PauseGame();
         }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
